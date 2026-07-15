ca383ce513025631a98f85b3b6cf75e9345707a8
26169upskill <26169upskill@student.wethinkcode.co.za>
2026-07-15 12:54:38 +0200

    added all my packages

--- src/test/java/basics/loginToNdosiWebsiteMultipleMethod.java
+++ src/test/java/basics/loginToNdosiWebsiteMultipleMethod.java
@@ -1,8 +1,9 @@
+package basics;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
