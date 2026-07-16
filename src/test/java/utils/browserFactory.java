3b3659a5962b906e939654ba9696653b3250c56a
Lydia Tlou <tiegotlou@gmail.com>
2026-07-16 17:48:51 +0200

    added browserFactory class

---
+++ src/test/java/utils/browserFactory.java
@@ -0,0 +1,40 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class browserFactory {
+
+
+    static WebDriver driver;
+
+    public static void launchBrowser(String browserName, String url) {
+
+        if (browserName.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+
+        } else if (browserName.equalsIgnoreCase("edge")) {
+            driver = new SafariDriver();
+
+        } else {
+            driver = new EdgeDriver();
+        }
+        driver.manage().window().maximize();
+        driver.get(url);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+    @Test
+    public void provider(){
+        launchBrowser("firefox","https://ndosisimplifiedautomation.vercel.app");
+    }
+}
