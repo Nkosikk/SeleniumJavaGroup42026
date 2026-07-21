51770395bdc86c3169e80bb26c8f6f3f01a74199
itumelengkgosoane <kgosoanei@gmail.com>
2026-07-21 22:19:40 +0200

    added home page and login page

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+   WebDriver driver;
+
+
+        //Finding elements
+        By homeScreen_xpath = By.xpath("//button[@class='nav-item ']/span[contains(text(),'Home')]");
+
+        By mainLoginButton = By.XPath("//div[@class='nav-user-section ']/button/span[contains(text(), 'Logon')]");
+
+        //Constructor
+            public HomePage(WebDriver driver){
+                this.driver = driver;
+
+    }
+            public void verifyHomeScreenContent(){
+                driver.findElement(homeScreen_xpath).isDisplayed();
+
+    }
+            public void clickMainLoginButton(){
+                driver.findElement(mainLoginButton).click();
+
+    }
+
+
+
+
+}
