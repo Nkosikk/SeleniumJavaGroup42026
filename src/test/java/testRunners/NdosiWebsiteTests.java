7ea4e18e9d206c241a7e78cf941cb33fbb235a87
itumelengkgosoane <kgosoanei@gmail.com>
2026-07-23 00:28:55 +0200

    Added browser factory class

--- src/test/java/testRunners/NdosiWebsiteTests.java
+++ src/test/java/testRunners/NdosiWebsiteTests.java
@@ -1,8 +1,5 @@
 package testRunners;
-
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
-import pages.HomePage;
 import utils.Base;
 
 public class NdosiWebsiteTests extends Base {
