b43e7861611393df3fd652f276def7b16ceea91a
Lydia Tlou <tiegotlou@gmail.com>
2026-07-23 17:25:24 +0200

    added ndosiTestsWebsite

---
+++ src/test/java/testRunners/ndosiTestsWebsite.java
@@ -0,0 +1,19 @@
+package testRunners;
+
+import org.testng.annotations.Test;
+
+public class ndosiTestsWebsite extends utils.base {
+
+    @Test (priority = 1)
+    public void verifyHomeContentTest() {
+        homePage.verifyHomeScreenContent();
+    }
+
+    @Test(priority = 2)
+    public void clickMainLoginButtonTest() {
+        homePage.clickMainLoginButton();
+        loginPage.enterUserName("Titi@gmail.com");
+        loginPage.enterPassword("tlou@97LT");
+        loginPage.clickSubmitButton();
+    }
+}
