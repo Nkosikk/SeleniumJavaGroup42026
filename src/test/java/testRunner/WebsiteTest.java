6cb5e74ca498f55cbb89083f77da611402eb9624
BrendaMahinga <shiluvam@gmail.com>
2026-07-27 21:04:36 +0200

    added code to classes

--- src/test/java/testRunner/WebsiteTest.java
+++ src/test/java/testRunner/WebsiteTest.java
@@ -1,4 +1,43 @@
 package testRunner;
 
-public class WebsiteTest {
+import org.testng.annotations.Test;
+import utils.Base;
+
+public class WebsiteTest extends Base {
+
+//
+//    @Test(priority = 1)
+//    public void verifyHomeContentTest(){
+//        homePage.verifyHomeScreenContent();
+//    }
+//
+//    @Test(priority = 2)
+//    public void submitLoginDetailsTest(){
+//        homePage.clickMainLoginButton();
+//        loginPage.enterUsername("nkwanyana@gmail.com");
+//        loginPage.enterPassword("#12345678");
+//        loginPage.loginButton();
+//    }
+//
+//    @Test(dependsOnMethods = "submitLoginDetailsTest")
+//    public void verifyLoginFlowIsSuccessfulTest(){
+//        dashboardPage.verifyDashboardContent();
+//    }
+//
+//    @AfterClass
+//    public void closeBrowser(){
+//        driver.quit();
+//    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
