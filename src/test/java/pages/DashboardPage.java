6659e88847f06603e00d655dc3654683a12ad064
BrendaMahinga <shiluvam@gmail.com>
2026-07-27 20:51:50 +0200

    added code to classes

--- src/test/java/pages/DashboardPage.java
+++ src/test/java/pages/DashboardPage.java
@@ -1,4 +1,31 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class DashboardPage {
+
+    WebDriver driver;
+
+    By welcomePage_xpath = By.xpath("//h2");
+
+    public DashboardPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void verifyDashboardContent(){
+        driver.findElement(welcomePage_xpath).isDisplayed();
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
