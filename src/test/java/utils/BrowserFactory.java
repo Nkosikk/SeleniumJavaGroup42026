63b8fd5ab41d9ad2800e46a178b5f185d88887c0
Thato Mchunu <thatomchunu64@gmail.com>
2026-09-08 00:57:16 +0200

    created SeleniumPipeline.yml, updated BrowserFactory code(screen maximising/dimensions, headless chrome option), updated testng.xml code, deleted NdosiTestings.java

--- src/test/java/utils/BrowserFactory.java
+++ src/test/java/utils/BrowserFactory.java
@@ -1,10 +1,13 @@
 package utils;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
@@ -14,31 +17,51 @@ public class BrowserFactory {
     static WebDriver driverFact;
 
 
-    public static WebDriver launchBrowser(String browserName, String url) {
-
-        System.out.println("Launching Browser.....");
+    public static WebDriver launchBrowser(String browserName, String websiteUrl) {
 
         if (browserName.equalsIgnoreCase("Chrome")) {
 
             ChromeOptions options = new ChromeOptions();
-            //options.addArguments("--disable-notifications");// blocks the alert messages
-            options.addArguments("--incognito"); // opens incognito mode
+            options.addArguments("--incognito");
+            options.addArguments("--disable-notifications");
+            options.addArguments("--headless=new");
 
             driverFact = new ChromeDriver(options);
+            //driverFact.manage().window().maximize()
+            driverFact.manage().window().setSize(new Dimension(1920, 1080));
 
-        } else if (browserName.equalsIgnoreCase("Firefox")) {
-            driverFact = new FirefoxDriver();
 
-        } else if (browserName.equalsIgnoreCase("Safari")) {
+            driverFact.get(websiteUrl);
+            driverFact.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        } else if (browserName.equalsIgnoreCase("safari")) {
+
             driverFact = new SafariDriver();
+            driverFact.manage().window().maximize();
+            driverFact.get(websiteUrl);
+            driverFact.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+
+            FirefoxOptions fireOptions = new FirefoxOptions();
+            fireOptions.addArguments("--incognito");
+            fireOptions.addArguments("--disable-notifications");
+
+            driverFact = new FirefoxDriver(fireOptions);
+            driverFact.manage().window().maximize();
+            driverFact.get(websiteUrl);
+            driverFact.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         } else {
-            driverFact = new EdgeDriver();
+            EdgeOptions edgeOpt = new EdgeOptions();
+            edgeOpt.addArguments("--incognito");
+            edgeOpt.addArguments("--disable-notifications");
+
+            driverFact = new EdgeDriver(edgeOpt);
+            driverFact.manage().window().maximize();
+            driverFact.get(websiteUrl);
+            driverFact.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
-        driverFact.manage().window().maximize();
-        driverFact.get(url);
-        driverFact.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        System.out.println("Browser Launched!");
 
         return driverFact;
     }
